28d05b5f4916c41921d33350522f805ece3a3152
wConnor <connorg7@posteo.net>
2020-08-03 03:47:04 +0100

    implemented new functions with placeholder arguments

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main(int argc, char *argv[])
   initscr();
   cbreak();
   noecho();
-  
-  
-  
-
+  std::vector<std::string> front = {"Hallo", "Salut", "Français"};
+  std::vector<std::string> back = {"Hello", "Hi", "French"};
+  Session *session = new Session();
+  session->setFront(front);
+  session->setBack(back);
+  session->outputContents();
 
   
   getch();
