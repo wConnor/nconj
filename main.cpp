de9631c14f3b367eda090cbb99b2d6970a5526a6
wConnor <connorg7@posteo.net>
2020-08-07 11:27:40 +0100

    now passes infinitive to session->begin()

--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@ int main(int argc, char *argv[])
   start_color();
 
   Menu *menu = new Menu();
-  
-  std::vector<std::string> front = {"oneF", "twoF", "threeF"};
-  std::vector<std::string> back = {"oneB", "twoB", "threeB"};
+
+  std::string infinitive = "être";
+  std::vector<std::string> front = {"je", "tu", "il, elle, on", "nous", "vous", "ils/elles"};
+  std::vector<std::string> back = {"suis", "es", "est", "sommes", "êtes", "sont"};
 
   while (1) {
 	if (menu->print() >= 0) {
 	  Session *session = new Session();
-	  session->begin(front, back);
+	  session->begin(front, back, infinitive);
 	} else {
 	  break;
 	}
