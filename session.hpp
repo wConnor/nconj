7c8378fc31ef55852e3b8a790ec9c80606d685f5
wConnor <connorg7@posteo.net>
2020-08-08 15:50:24 +0100

    indentation cleanup

--- session.hpp
+++ session.hpp
@@ -1,20 +1,21 @@
 #ifndef SESSION_HPP
 #define SESSION_HPP
 
-#include <ncurses.h>
-#include <vector>
-#include <string>
-#include <memory>
 #include <algorithm>
+#include <memory>
+#include <ncurses.h>
+#include <string>
+#include <vector>
 
-class Session
-{
+class Session {
 private:
-  
 public:
   Session();
-  void begin(const std::vector<std::string> &front, const std::vector<std::string> &back, const std::string &infinitive);
-  void results(const int &score, const int &total, const std::vector<std::string> incorrect);
+  void begin(const std::vector<std::string> &front,
+             const std::vector<std::string> &back,
+             const std::string &infinitive);
+  void results(const int &score, const int &total,
+               const std::vector<std::string> incorrect);
   virtual ~Session();
 };
 
