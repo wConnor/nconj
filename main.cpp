0854fc1a75ebbc3a8b0b731c73b5a02b3b178b86
wConnor <connorg7@posteo.net>
2020-08-09 17:50:19 +0100

    implemented new deck class

--- main.cpp
+++ main.cpp
@@ -1,107 +1,32 @@
+#include "deck.hpp"
 #include "menu.hpp"
 #include "session.hpp"
 #include <filesystem>
-#include <fstream>
 
 int main(int argc, char *argv[]) {
+  Deck *deck = new Deck();
 
   /* checks to see whether or not the data directory
          exists and, if it does not, creates both it and
          the decks.ncj file with some example french
          conjugation.
   */
-  if (!std::filesystem::exists("./data/")) {
-    std::filesystem::create_directory("./data/");
-    std::ofstream *file = new std::ofstream("./data/decks.ncj");
-    *file << "%être$%je,tu,il/elle,nous,vous,ils/"
-             "elles$%suis,es,est,sommes,êtes,sont$"
-          << std::endl;
-    *file
-        << "%avoir$%j',tu,il/elle,nous,vous,ils/elles$%ai,as,a,avons,avez,ont$"
-        << std::endl;
-    file->close();
-    delete file;
+  if (!std::filesystem::exists("./decks/")) {
+    std::filesystem::create_directory("./decks/");
+
+    deck->create("être",
+                 std::vector<std::string>{"je,tu,il/elle,nous,vous,ils/elles"},
+                 std::vector<std::string>{"suis,es,est,sommes,êtes,sont"});
   }
 
-  std::ifstream decksFile("./data/decks.ncj");
-  std::string line;
-
-  std::string infTemp = "";
-  std::string frontTemp = "";
-  std::string backTemp = "";
-
-  std::vector<std::string> infinitives = {};
-  std::vector<std::vector<std::string>> front;
-  std::vector<std::vector<std::string>> back;
-  std::vector<std::string> *fileContents = new std::vector<std::string>();
-
-  // reads each line of decks.ncj into the fileContents vector.
-  if (decksFile.good()) {
-    while (std::getline(decksFile, line)) {
-      if (line.size() > 0) {
-        fileContents->push_back(line);
-      }
-    }
+  std::vector<std::string> availableDecks = {};
+  for (const auto &e : std::filesystem::directory_iterator("./decks/")) {
+    availableDecks.push_back(e.path());
   }
 
-  /* iterates through the data stored in fileContents (line
-         by line), extracting the contents between the starting
-         delimiter (%) and the end delimiter ($). these are then stored
-         in a temporary variable and then pushed back to
-         their respective vectors.
-  */
-  std::vector<std::string> tempVF = {};
-  std::vector<std::string> tempVB = {};
-  int counter = 0;
-  for (auto &c : *fileContents) {
-	infTemp = "";
-	frontTemp = "";
-	backTemp = "";
+  deck->setFile("être");
+  deck->readContents();
 
-    auto start = c.find('%');
-    auto end = c.find('$');
-
-    infTemp = c.substr(start + 1, end - (start + 1));
-    c.erase(start, end + 1);
-    start = c.find('%');
-    end = c.find('$');
-
-    frontTemp = c.substr(start + 1, end - (start + 1));
-    std::stringstream frontStream(frontTemp);
-
-    while (frontStream.good()) {
-      std::string substr;
-      std::getline(frontStream, substr, ',');
-	  tempVF.push_back(substr);
-    }
-	front.push_back(tempVF);
-	tempVF.clear();
-
-    c.erase(start, end + 1);
-    start = c.find('%');
-    end = c.find('$');
-    backTemp = c.substr(start + 1, end - (start + 1));
-
-    std::stringstream backStream(backTemp);
-
-    while (backStream.good()) {
-      std::string substr;
-      std::getline(backStream, substr, ',');
-	  tempVB.push_back(substr);
-    }
-	back.push_back(tempVB);
-	tempVB.clear();
-
-    backTemp = c.substr(start + 1, end - (start + 1));
-    c.erase(start, end + 1);
-
-    infinitives.push_back(infTemp);
-	++counter;
-	c.erase(0,c.find('$'));
-  }
-
-  delete fileContents;
-  decksFile.close();
   initscr();
   cbreak();
   start_color();
@@ -109,10 +34,10 @@ int main(int argc, char *argv[]) {
   Menu *menu = new Menu();
   int selectedDeck;
   while (1) {
-    selectedDeck = menu->print(infinitives);
+    selectedDeck = menu->print(availableDecks);
     if (selectedDeck >= 0) {
       Session *session = new Session();
-      session->begin(front[selectedDeck], back[selectedDeck], infinitives[selectedDeck]);
+      session->begin(deck->getFront(), deck->getBack(), deck->getName());
       delete session;
     } else {
       break;
