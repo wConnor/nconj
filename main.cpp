a09fd76cd19022cfd841726258d6604be5a33e0d
wConnor <connorg7@posteo.net>
2020-08-09 13:42:32 +0100

    added reading from decks.ncj!!! very inefficient, but agile ;)

--- main.cpp
+++ main.cpp
@@ -5,8 +5,22 @@
 
 int main(int argc, char *argv[]) {
 
+  /* checks to see whether or not the data directory
+         exists and, if it does not, creates both it and
+         the decks.ncj file with some example french
+         conjugation.
+  */
   if (!std::filesystem::exists("./data/")) {
     std::filesystem::create_directory("./data/");
+    std::ofstream *file = new std::ofstream("./data/decks.ncj");
+    *file << "%être$%je,tu,il/elle,nous,vous,ils/"
+             "elles$%suis,es,est,sommes,êtes,sont$"
+          << std::endl;
+    *file
+        << "%avoir$%j',tu,il/elle,nous,vous,ils/elles$%ai,as,a,avons,avez,ont$"
+        << std::endl;
+    file->close();
+    delete file;
   }
 
   std::ifstream decksFile("./data/decks.ncj");
@@ -17,8 +31,8 @@ int main(int argc, char *argv[]) {
   std::string backTemp = "";
 
   std::vector<std::string> infinitives = {};
-  std::vector<std::string> front = {};
-  std::vector<std::string> back = {};
+  std::vector<std::vector<std::string>> front;
+  std::vector<std::vector<std::string>> back;
   std::vector<std::string> *fileContents = new std::vector<std::string>();
 
   // reads each line of decks.ncj into the fileContents vector.
@@ -30,26 +44,60 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  /* iterates through the data stored in fileContents,
-	 extracting the contents between the starting delimiter
-	 (%) and the end delimiter ($). these are then stored
-	 in a temporary variable and then pushed back to
-	 their respective vectors.
+  /* iterates through the data stored in fileContents (line
+         by line), extracting the contents between the starting
+         delimiter (%) and the end delimiter ($). these are then stored
+         in a temporary variable and then pushed back to
+         their respective vectors.
   */
+  std::vector<std::string> tempVF = {};
+  std::vector<std::string> tempVB = {};
+  int counter = 0;
   for (auto &c : *fileContents) {
+	infTemp = "";
+	frontTemp = "";
+	backTemp = "";
+
     auto start = c.find('%');
     auto end = c.find('$');
 
     infTemp = c.substr(start + 1, end - (start + 1));
     c.erase(start, end + 1);
+    start = c.find('%');
+    end = c.find('$');
+
     frontTemp = c.substr(start + 1, end - (start + 1));
+    std::stringstream frontStream(frontTemp);
+
+    while (frontStream.good()) {
+      std::string substr;
+      std::getline(frontStream, substr, ',');
+	  tempVF.push_back(substr);
+    }
+	front.push_back(tempVF);
+	tempVF.clear();
+
     c.erase(start, end + 1);
+    start = c.find('%');
+    end = c.find('$');
+    backTemp = c.substr(start + 1, end - (start + 1));
+
+    std::stringstream backStream(backTemp);
+
+    while (backStream.good()) {
+      std::string substr;
+      std::getline(backStream, substr, ',');
+	  tempVB.push_back(substr);
+    }
+	back.push_back(tempVB);
+	tempVB.clear();
+
     backTemp = c.substr(start + 1, end - (start + 1));
     c.erase(start, end + 1);
 
     infinitives.push_back(infTemp);
-    front.push_back(frontTemp);
-    back.push_back(backTemp);
+	++counter;
+	c.erase(0,c.find('$'));
   }
 
   delete fileContents;
@@ -64,7 +112,7 @@ int main(int argc, char *argv[]) {
     selectedDeck = menu->print(infinitives);
     if (selectedDeck >= 0) {
       Session *session = new Session();
-      session->begin(front, back, infinitives[selectedDeck]);
+      session->begin(front[selectedDeck], back[selectedDeck], infinitives[selectedDeck]);
       delete session;
     } else {
       break;
