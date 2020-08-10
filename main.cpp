0f19b9d26ab27b7906b5f668143de1aaac52f38e
wConnor <connorg7@posteo.net>
2020-08-10 14:40:46 +0100

    set locale and deals with return values from menu accordingly

--- main.cpp
+++ main.cpp
@@ -22,20 +22,8 @@ int main(int argc, char *argv[]) {
   }
 
   std::vector<std::string> availableDecks = {};
-  for (const auto &e : std::filesystem::directory_iterator("./decks/")) {
-    std::string tempName = "";
-    std::filesystem::path tempPath = e.path();
-    tempName = tempPath.string();
 
-    auto start = tempName.find_last_of('/');
-    tempName.erase(0, start + 1);
-
-    auto end = tempName.find_last_of('.');
-    tempName.erase(end, tempName.size() - 1);
-
-    availableDecks.push_back(tempName);
-  }
-
+  setlocale(LC_ALL, "");
   initscr();
   cbreak();
   start_color();
@@ -43,15 +31,37 @@ int main(int argc, char *argv[]) {
   Menu *menu = new Menu();
   int selectedDeck;
   while (1) {
+    availableDecks.clear();
+    cbreak();
+    for (const auto &e : std::filesystem::directory_iterator("./decks/")) {
+      std::string tempName = "";
+      std::filesystem::path tempPath = e.path();
+      tempName = tempPath.string();
+
+      auto start = tempName.find_last_of('/');
+      tempName.erase(0, start + 1);
+
+      auto end = tempName.find_last_of('.');
+      tempName.erase(end, tempName.size() - 1);
+
+      availableDecks.push_back(tempName);
+    }
+
     selectedDeck = menu->print(availableDecks);
-    if (selectedDeck >= 0) {
+    // create new deck
+    if (selectedDeck == -1) {
+      std::unique_ptr<Deck> tempDeck = std::make_unique<Deck>();
+      menu->newDeck(tempDeck);
+      // quit program
+    } else if (selectedDeck == -2) {
+      break;
+      // start session with chosen deck
+    } else if (selectedDeck >= 0) {
       std::unique_ptr<Deck> deck =
           std::make_unique<Deck>(availableDecks[selectedDeck]);
       std::unique_ptr<Session> session = std::make_unique<Session>();
       deck->readContents();
       session->begin(deck->getFront(), deck->getBack(), deck->getName());
-    } else {
-      break;
     }
   }
   delete menu;
